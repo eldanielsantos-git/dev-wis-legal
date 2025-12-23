80f2c704786b3016ea53631edad2edbb38ee9622
Daniel Santos <eldanielsantos@gmail.com>
2025-12-23 13:58:34 -0300

    Updated LoadingSpinner.tsx

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -13,9 +13,9 @@ export function LoadingSpinner({ size = 'md', color }: LoadingSpinnerProps = {})
   const colors = useMemo(() => getThemeColors(theme), [theme]);
 
   const sizeClasses = {
-    sm: 'w-8 h-8',
-    md: 'w-12 h-12',
-    lg: 'w-16 h-16'
+    sm: 'w-4 h-4',
+    md: 'w-6 h-6',
+    lg: 'w-8 h-8'
   };
 
   return (
